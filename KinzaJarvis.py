9873e57bd8e9be64beccc9065bc6cc87af5f836a
KinzaRehman <152224153+KinzaRehman@users.noreply.github.com>
2024-03-02 22:32:37 -0500

    Add files via upload

---
+++ KinzaJarvis.py
@@ -0,0 +1,52 @@
+import os
+import pyaudio
+import speech_recognition as sr
+from gtts import gTTS
+import openai
+import time
+import playsound
+
+api_key = "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"
+lang = 'en'
+openai.api_key = api_key
+
+def speak(text):
+    speech = gTTS(text=text, lang=lang, slow=False)
+    speech.save("output.mp3")
+    playsound.playsound("output.mp3")
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("I am listening...")
+        audio = r.listen(source)
+    return r.recognize_google(audio)
+
+exit_phrases = ["Exit", "Please Stop", "Stop", "End Now"]
+
+try:
+    speak("Go ahead and say something... ")
+    audio_text = get_audio()
+    print("You said:", audio_text)
+
+    for phrase in exit_phrases:
+        if phrase.lower() in audio_text.lower():
+            print("Stopping now...")
+            break
+
+    if "Jarvis" in audio_text:
+        words = audio_text.split()
+        new_string = ' '.join(words[1:])
+        print(new_string)
+        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo",
+                                                  messages=[{"role": "user", "content": audio_text}])
+        text = completion.choices[0].message.content
+        speak(text)
+
+except sr.UnknownValueError:
+    print("Could not understand audio....")
+except sr.RequestError as e:
+    print("Could not request results; {0}".format(e))
+except Exception as e:
+    print("Exception:", e)
+
